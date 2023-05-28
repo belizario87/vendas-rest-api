a599fea481e2f44bea5787bc4e687bf1581110d6
belizario87 <leonardobelizario88@gmail.com>
2023-05-28 18:47:47 -0300

    add ClienteNotFoundException

---
+++ src/main/java/br/com/belizario87/vendas/vendasapp/exception/ClienteNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.belizario87.vendas.vendasapp.exception;
+
+public class ClienteNotFoundException extends RuntimeException {
+    public ClienteNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
